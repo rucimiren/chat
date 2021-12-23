6d7b75f50e4158764c6d4042e73f96337f73d285
ext.xiaoqiang <ext.xiaoqiang@jd.com>
2021-12-23 16:39:45 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,53 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    es6: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    camelcase: 2, // 强制驼峰命名
+    quotes: [2, 'single'], // 强制使用单引号
+    eqeqeq: 'warn', // TODO: 后期强制
+    'block-scoped-var': 2, // 不能使用块级作用域内定义的变量
+    'array-bracket-spacing': [2, 'never'], // 指定数组的元素之间要以空格隔开
+    'comma-dangle': [2, 'always-multiline'], // 数组、对象末尾逗号
+    'comma-spacing': [2, { before: false, after: true }], // 逗号前后的空格
+    'comma-style': [2, 'last'], // 逗号在行尾
+    'vue/attributes-order': [
+      2,
+      {
+        // 属性顺序
+        order: [
+          'DEFINITION',
+          'LIST_RENDERING',
+          'CONDITIONALS',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          ['UNIQUE', 'SLOT'],
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'OTHER_ATTR',
+          'EVENTS',
+          'CONTENT',
+        ],
+        alphabetical: true,
+      },
+    ],
+    'vue/no-unused-components': 2,
+    'no-unused-vars': 2,
+    'no-case-declarations': 0,
+    'no-prototype-builtins': 0, // 直接使用 Object.prototypes 的内置属性
+    'standard/no-callback-literal': 0,
+    'no-throw-literal': 0,
+    'no-mixed-operators': 0, // TODO: remove popper.js
+    'node/no-deprecated-api': 0, // TODO: wtf
+    'vue/no-mutating-props': 0, // 信息管理-年度财报页面，修改子组件props报错
   },
-};
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,35 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Home from '@/views/home/index.vue';
 
 Vue.use(VueRouter);
 
+// 处理异常问题
+const originalPush = VueRouter.prototype.push;
+const originalReplace = VueRouter.prototype.replace;
+VueRouter.prototype.push = function (location) {
+  return originalPush.call(this, location).catch((err) => err);
+};
+VueRouter.prototype.replace = function (location) {
+  return originalReplace.call(this, location).catch((err) => err);
+};
+
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'home',
     component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  document.title = '肖肖';
+  next();
 });
 
 export default router;
